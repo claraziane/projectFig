90cb98a2fe77251061d60f27342dd5cf24093290
claraziane <clara.ziane@gmail.com>
2024-06-13 17:31:27 -0400

    vImproved

--- plotScatter.m
+++ plotScatter.m
@@ -1,36 +1,46 @@
-function fig_resultsScatter(data, xLabel, yLabel)
+function plotScatter(data, Comparison, xLabel, yLabel)
 
 % Define colors for plotting
 addpath('/Users/claraziane/Documents/Académique/Informatique/MATLAB/rgb/'); %To draw figures
 
-for iLabel = 1:length(xLabel)
-    if strcmpi(xLabel{iLabel}(1:6), 'uncued')
-        Colors(iLabel,:)     = rgb('LightBlue');
-        ColorMeans(iLabel,:) = rgb('DodgerBlue');
-        xGroup(iLabel,1:2)   = iLabel-1 + 0.7;
-    else
-        Colors(iLabel,:)     = rgb('NavajoWhite');
-        ColorMeans(iLabel,:) = rgb('DarkOrange');
-        xGroup(iLabel,1:2)   = iLabel-1 + 0.7;
+iPlot  = 1;
+for iCond = 1:size(xLabel,1)
+    for iComparison = 1:length(Comparison)
+        if strcmpi(Comparison{iComparison}, 'uncued') || strcmpi(Comparison{iComparison}, 'ST')
+            Colors(iPlot,:)     = rgb('LightBlue');
+            ColorMeans(iPlot,:) = rgb('DodgerBlue');
+            xGroup(iPlot,1:2)   = iPlot - 0.3;
+        else
+            Colors(iPlot,:)     = rgb('NavajoWhite');
+            ColorMeans(iPlot,:) = rgb('DarkOrange');
+            xGroup(iPlot,1:2)   = iPlot - 0.2;
+        end
+        iPlot = iPlot+1;
     end
 end
 Colors(end+1,:) = rgb('LightGray');
 
-xTicks(:,1) = randi([1, 40], size(data,1), 1);
-xTicks(:,1) = 0.8 + (xTicks(:,1)/100);
+xTicks(:,1) = randi([1, 20], size(data,1), 1);
+xTicks(:,1) = 0.9 + (xTicks(:,1)/100);
 
 for iCond = 2:size(data,2)
-    xTicks(:,iCond) = xTicks(:,iCond-1)+1;
+    if ismember(iCond, [3 5 7 9])
+        xTicks(:,iCond) = xTicks(:,iCond-1)+1.5;
+    else
+        xTicks(:,iCond) = xTicks(:,iCond-1)+0.5;
+    end
 end
 
-figure;
 % Plot horizontal lines
-plot(xTicks', data', 'Color', Colors(end,:), 'LineWidth', 2); hold on;
+figure;
+for iCond = 1:2:size(data,2)
+    plot(xTicks(:,iCond:iCond+1)', data(:,iCond:iCond+1)', 'Color', Colors(end,:), 'LineWidth', 2); hold on;
+end
 
 % Define x axis
 ax = gca;
 set(ax, 'xlim',  [0 size(data,2)+1])
-set(ax, 'xtick', [1:size(data,2)])
+set(ax, 'xtick', [1.25:2:size(data,2)])
 set(ax, 'xticklabel', xLabel);
 set(ax, 'FontWeight', 'bold', 'FontSize', 20);
 ylabel(yLabel)
@@ -56,15 +66,15 @@ for iGroup = 1:size(data,2)
     scatter(xGroup(iGroup,1), nanmean(data(:,iGroup)), 500, 'MarkerEdgeColor', 'k', 'MarkerFaceColor', ColorMeans(iGroup,:), 'MarkerFaceAlpha', 0.8);
     hold on;
    
-    set(gca, 'XTick', [1.2:2:(length(xLabel)+0.2)])
+%     set(gca, 'XTick', [1.2:2:(length(xLabel)+0.2)])
 
 end
 
-xLabel = xLabel(1:2:end);
-for iLabel = 1:length(xLabel)
-    xLabelFinal{iLabel} = xLabel(iLabel);
-    xLabelFinal{iLabel} = xLabelFinal{1,iLabel}{1, 1}(5:end);
-end
-set(gca, 'XTickLabel', xLabelFinal)
+% xLabel = xLabel(1:2:end);
+% for iLabel = 1:length(xLabel)
+%     xLabelFinal{iLabel} = xLabel(iLabel);
+%     xLabelFinal{iLabel} = xLabelFinal{1,iLabel}{1, 1}(5:end);
+% end
+% set(gca, 'XTickLabel', xLabelFinal)
 
 end
--- plotScatterCI.m
+++ plotScatterCI.m
@@ -1,43 +1,59 @@
-function fig_resultsScatterCI(data, CI, xLabels, yLabel)
+function plotScatterCI(data, CI, Comparison, xLabel, yLabel)
 
 % Define colors for plotting
-addpath('/Volumes/Seagate/project_rhythmicBrain/Toolbox/rgb'); %To draw figures
+addpath('/Users/claraziane/Documents/Académique/Informatique/MATLAB/rgb/'); %To draw figures
 
-if size(data,2) == 2
-    Colors = [rgb('DarkOrange'); rgb('DodgerBlue'); rgb('Silver')];
-    xGroup = [0.6 0.6; 2.4 2.4];
-else
-    Colors = [rgb('DarkOrange'); rgb('DarkOrange'); rgb('DarkOrange'); rgb('DodgerBlue'); rgb('Silver')];
-    xGroup = [0.7 0.7; 1.7 1.7; 2.7 2.7; 3.7 3.7];
+iPlot  = 1;
+for iCond = 1:size(xLabel,1)
+    for iComparison = 1:length(Comparison)
+        if strcmpi(Comparison{iComparison}, 'uncued') || strcmpi(Comparison{iComparison}, 'ST')
+            Colors(iPlot,:)     = rgb('LightBlue');
+            ColorMeans(iPlot,:) = rgb('DodgerBlue');
+            xGroup(iPlot,1:2)   = iPlot - 0.3;
+        else
+            Colors(iPlot,:)     = rgb('NavajoWhite');
+            ColorMeans(iPlot,:) = rgb('DarkOrange');
+            xGroup(iPlot,1:2)   = iPlot - 0.2;
+        end
+        iPlot = iPlot+1;
+    end
 end
+Colors(end+1,:) = rgb('LightGray');
 
-xTicks(:,1) = randi([1, 40], size(data,1), 1);
-xTicks(:,1) = 0.8 + (xTicks(:,1)/100);
+xTicks(:,1) = randi([1, 20], size(data,1), 1);
+xTicks(:,1) = 0.9 + (xTicks(:,1)/100);
 
 for iCond = 2:size(data,2)
-    xTicks(:,iCond) = xTicks(:,iCond-1)+1;
+    if ismember(iCond, [3 5 7 9])
+        xTicks(:,iCond) = xTicks(:,iCond-1)+1.5;
+    else
+        xTicks(:,iCond) = xTicks(:,iCond-1)+0.5;
+    end
 end
 
-figure;
-
 % Plot horizontal lines
-plot(xTicks', data', 'Color', Colors(end,:), 'LineWidth', 2); hold on;
+figure;
+for iCond = 1:2:size(data,2)
+    plot(xTicks(:,iCond:iCond+1)', data(:,iCond:iCond+1)', 'Color', Colors(end,:), 'LineWidth', 2); hold on;
+end
 
 % Define x axis
 ax = gca;
 set(ax, 'xlim',  [0 size(data,2)+1])
-set(ax, 'xtick', [1:size(data,2)])
-set(ax, 'xticklabel', xLabels);
+set(ax, 'xtick', [1.25:2:size(data,2)])
+set(ax, 'xticklabel', xLabel);
 set(ax, 'FontWeight', 'bold', 'FontSize', 20);
 ylabel(yLabel)
+% title(figTitle);
 hold on;
 
 for iGroup = 1:size(data,2)
 
      % Plot 95% CI bars for each participant
-    for iParticipant = 1:length(data)
+    for iParticipant = 1:size(data,1)
         plot([xTicks(iParticipant,iGroup) xTicks(iParticipant,iGroup)], CI(iParticipant,:,iGroup), '-', 'Color', Colors(iGroup,:), 'LineWidth', 2)
     end
+
     % Plot scatter points for each participant
     scatter(xTicks(:,iGroup), data(:,iGroup), 200,  'MarkerEdgeColor', 'w', 'MarkerFaceColor', 'w');
     scatter(xTicks(:,iGroup), data(:,iGroup), 200,  'MarkerEdgeColor', Colors(end,:), 'MarkerFaceColor', Colors(iGroup,:), 'MarkerFaceAlpha', .5);
@@ -48,11 +64,11 @@ for iGroup = 1:size(data,2)
     groupCI = mean(data(:,iGroup)) + t * groupSEM;
 
     % Plot group CIs
-    plot(xGroup(iGroup, 1:2), groupCI, '-', 'Color', Colors(iGroup,:), 'LineWidth', 5)
+    plot(xGroup(iGroup, 1:2), groupCI, '-', 'Color', ColorMeans(iGroup,:), 'LineWidth', 5)
 
     % Plot group mean
     scatter(xGroup(iGroup,1), mean(data(:,iGroup)), 500, 'w', 'filled');
-    scatter(xGroup(iGroup,1), mean(data(:,iGroup)), 500, 'MarkerEdgeColor', 'k', 'MarkerFaceColor', Colors(iGroup,:), 'MarkerFaceAlpha', 0.8);
+    scatter(xGroup(iGroup,1), mean(data(:,iGroup)), 500, 'MarkerEdgeColor', 'k', 'MarkerFaceColor', ColorMeans(iGroup,:), 'MarkerFaceAlpha', 0.8);
     hold on;
 
 end
